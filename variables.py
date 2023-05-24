973752a8e30300950d16da0d1c8b18d58e8841ed
PRASAD GOWDA <107786878+ching-128@users.noreply.github.com>
2023-05-25 00:19:07 +0530

    file uploaded.

---
+++ variables.py
@@ -0,0 +1,28 @@
+a = 10
+print(a)
+
+b = 30
+print(b)
+print(a * b)
+
+print(id(a))
+string = "hello world"
+print(string)
+
+_underscore = "_hello"
+print(_underscore)
+
+x, y, z = 20, 35, 40
+print(x, y, z)
+
+# destructuring of array or unpacking of array
+num = [50, 40, 30]
+a, b, c = num
+print(c, b, a)
+
+# conditions for creating variables.
+# A variable name must start with a letter or the underscore character
+# A variable name cannot start with a number
+# A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+# Variable names are case-sensitive (age, Age and AGE are three different variables)
+# A variable name cannot be any of the Python keywords.
